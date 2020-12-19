caba6d3aae6eb690b581b676a2957ec7e45e768f
Heilrave <anisimovtoosad@gmail.com>
2020-12-19 16:49:50 +0400

    Commit (GUI)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Parser parser = new Parser();
+        Parser parser = new Parser(1);
         /*SwingUtilities.invokeLater(new Runnable(){
 
             @Override
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -4,7 +4,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -15,10 +14,19 @@ public class Parser {
     private static final Set<String> SET_LINKS = new HashSet<>();
     private Document document;
     private static final String GEN_URL = "https://rb.ru/sitemap-news.xml";
-    private static int sleepTimer = 40 * 60000; //in minutes
+    private int sleepTimer; //in minutes
     private static final String FILE_PATH = "listNews.txt";
-    private static boolean stateFill = false;
+    private static BufferedWriter writer;
+    private static int currentState; // 1 - все ссылки сохранены в txt, 2 - вся информация в БД
+    private final int THREADS = 8;
 
+    static {
+        try {
+            writer = new BufferedWriter(new FileWriter(FILE_PATH, true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void setSleepTimer(int time) {
         sleepTimer = time * 60000;
@@ -27,42 +35,97 @@ public class Parser {
     //TODO первичное наполнение, наблюдение
 
     public void primFill() throws IOException {
-        Elements links = document.select("url > loc");
-        for (Element div : links) {
-            String str = div.text();
-            String[] str1 = str.split("/"); //3 элемент
-            if (str1[3].equals("news") || str1[3].equals("story") || str1[3].equals("opinion")
-                    || str1[3].equals("interview")) {
-                SET_LINKS.add(div.text());
-            }
+        ExecutorService pool = Executors.newFixedThreadPool(THREADS);
+        Elements links = document.select("loc");
+        for (Element link: links){
+            pool.execute(() -> {
+                try {
+                    formLink(link);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         }
-
-        for (String str : SET_LINKS) {
-            System.out.println(str);
-            WebSite webSite = new WebSite(str);
-            webSite.printInfo();
+        pool.shutdown();
+        try {
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        setFlagTxt('1'); //все необходимые ссылки получены в файл
+        currentState = 1;
 
-        fillTxt();
+
+        if (SET_LINKS.isEmpty()) {
+            fillSet();
+        }
+        getInfoFields();
+        setFlagTxt('2'); //база данных наполнена информацией
+        currentState = 2;
     }
 
-    public void observation() {
+    /*public void observation() {
         if (SET_LINKS.isEmpty()) {
 
         }
+    }*/
+
+    synchronized private void addLinkTxt(String link) throws IOException {
+        writer.write(link + "\n");
     }
 
-    private void fillTxt() throws IOException {
+    private void setFlagTxt(char unit) throws IOException {
+        BufferedWriter writerF = new BufferedWriter(new FileWriter("listNews.txt"));
+        writerF.write(unit + "\n");
+        for (String str : SET_LINKS) {
+            writerF.write(str + "\n");
+        }
+        writerF.close();
+    }
+
+    private void getInfoFields() {
+        ExecutorService pool = Executors.newFixedThreadPool(THREADS);
+        for (String str : SET_LINKS) {
+            pool.execute(() -> {
+                try {
+                    WebSite webSite = new WebSite(str);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        pool.shutdown();
+        try {
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillSet() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH));
+        String str = reader.readLine();
+        if (str.equals("1") || str.equals("2")) {
+            str = reader.readLine();
+        }
+        while (str != null) {
+            SET_LINKS.add(str);
+            str = reader.readLine();
+        }
+        reader.close();
+    }
+
+    /*private void fillTxt() throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH));
         for (String str: SET_LINKS) {
             writer.write(str);
             writer.write("\n");
         }
         writer.close();
-        stateFill = true;
-    }
+        //stateFill = true;
+    }*/
 
-    private void inter() throws IOException, InterruptedException {
+    /*private void inter() throws IOException, InterruptedException {
         while (true) {
             Document document = Jsoup.connect(GEN_URL)
                     .userAgent("Chrome/4.0.249.0")
@@ -85,9 +148,9 @@ public class Parser {
 
             Thread.sleep(sleepTimer);
         }
-    }
+    }*/
 
-    private void updateTxt() throws IOException, InterruptedException {
+    /*private void updateTxt() throws IOException, InterruptedException {
         if (!SET_LINKS.isEmpty()) {
             inter();
         }
@@ -98,56 +161,35 @@ public class Parser {
                 SET_LINKS.add(str);
                 str = reader.readLine();
             }
-            stateFill = true;
+            //stateFill = true;
         }
+    }*/
 
-    }
-
-    private void printLink(Element link) throws IOException {
+    private void formLink(Element link) throws IOException {
         this.document = Jsoup.connect(link.text())
                 .userAgent("Chrome/4.0.249.0")
                 .referrer("http://www.google.com")
                 .get();
+
         Elements once = document.select("url > loc");
         for (Element onCE : once) {
-            System.out.println(onCE.text());
+            String[] arrr = onCE.text().split("/");
             synchronized (this) {
-                SET_LINKS.add(onCE.text());
+                if (!arrr[3].equals("tag")) {
+                    addLinkTxt(onCE.text());
+                    SET_LINKS.add(onCE.text());
+                }
             }
         }
-        //fillTxt();
     }
 
-    public Parser() throws IOException {
+    public Parser(int time) throws IOException {
+        //TODO проверки на заполненность txt
         this.document = Jsoup.connect("https://rb.ru/sitemap.xml")
                 .userAgent("Chrome/4.0.249.0")
                 .referrer("http://www.google.com")
                 .get();
-
-
-        final int THREADS = 8;
-        ExecutorService pool = Executors.newFixedThreadPool(THREADS);
-        Elements links = document.select("loc");
-        for (Element link: links){
-            pool.execute(() -> {
-                try {
-                    printLink(link);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        pool.shutdown();
-        try {
-            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("FINALLY");
-
-        fillTxt();
-
+        setSleepTimer(time);
     }
 
 }
