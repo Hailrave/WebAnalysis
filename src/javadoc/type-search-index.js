f353e3b64ac49402a1407d47d5a83bfbf2c79c63
Heilrave <anisimovtoosad@gmail.com>
2020-12-22 15:39:52 +0400

    Commit (finally)

---
+++ src/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Break_Window"},{"p":"<Unnamed>","l":"Data_Base"},{"p":"<Unnamed>","l":"End_Window"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Parser"},{"p":"<Unnamed>","l":"StartupWindow"},{"p":"<Unnamed>","l":"Timer_Window"},{"p":"<Unnamed>","l":"WebSite"}]
